1099ca6ee8081d291af85467f119546a6894b7a7
RoomanArhamSoft <muhammad.rooman@arhamsoft.org>
2025-01-15 17:52:16 +0500

    remove exta comments and large  function code convert to small code

--- components/SignIn/SignIn.js
+++ components/SignIn/SignIn.js
@@ -31,12 +31,10 @@ export default function SignIn() {
   const fullPhoneNumber = `${selectedCountryCode}${phoneNumber.trim()}`;
   
   useEffect(() => {
-    // Extract the language from the URL path or default to "en"
     const lang = currentPath.split("/")[1] || "en";
     setLanguage(lang);
   
     if (token) {
-      // Redirect to the event page for the detected language
       const redirectTimeout = setTimeout(() => {
        const loginURL =
      process.env.NEXT_PUBLIC_NODE_ENV === "development"
@@ -45,31 +43,23 @@ export default function SignIn() {
      router.push(loginURL);
      }, 1000);
   
-      // Cleanup timeout to avoid memory leaks
       return () => clearTimeout(redirectTimeout);
     }
-     setFirstLoader(false); // Stop the loader irrespective of token presence
+     setFirstLoader(false); 
   }, [currentPath, router, token]);
 
-
   const handleSendOTP = async () => {
-    // Validate phone number presence
     if (!phoneNumber) {
       setErrorMessage(t("phone_number_is_required"));
       return;
     }
   
-    // Validate phone number length
     if (phoneNumber.length < 8 || phoneNumber.length > 16) {
       setErrorMessage(t("phone_number_must_be_between_8_and_16_digits"));
       return;
     }
-  
-    // Indicate OTP sending process has started
     setIsOtpSent(true);
-  
     try {
-      // API call to generate OTP
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_BASE_API_FRONT}/auth/generate-otp`,
         {
@@ -77,24 +67,20 @@ export default function SignIn() {
           userExist: 1,
         }
       );
-  
-      // Handle successful OTP generation
       if (response?.data?.status === 1) {
-        setOtpGenerated(true); // Show OTP input field
+        setOtpGenerated(true); 
         setOtpMessage(response.data.message || "");
         toast.success(
           language === "en" ? response.data.message : response.data.message_ar
         );
         setErrorMessage("");
       } 
-      // Handle specific error message related to WhatsApp
       else if (response?.data?.message.includes("Failed to send WhatsApp message")) {
         toast.error(
           language === "en" ? "Invalid phone number" : "رقم الهاتف غير صالح"
         );
         setIsOtpSent(false);
       } 
-      // Handle general errors
       else {
         toast.error(
           language === "en" ? response.data.message : response.data.message_ar
@@ -120,7 +106,6 @@ export default function SignIn() {
           otpCode, 
         }
       );
-      // OTP Verified successfully
       if (response?.data?.success) {
         toast.success(language === "en" ? response.data.message : response.data.message_ar );
         setErrorVerifyMessage("");
@@ -142,17 +127,16 @@ export default function SignIn() {
       setErrorVerifyMessage(t("OTP_is_required"));
       return;
   }
-   // Validate phone number
+
     if (!phoneNumber || phoneNumber.trim() === "") {
       setErrorMessage(t("phone_number_is_required"));
       return;
   }
-  // Validate the phone number length (between 8 and 16 digits)
+
   if (phoneNumber.length < 8 || phoneNumber.length > 16) {
     setErrorMessage(t("phone_number_must_be_between_8_and_16_digits"));
-    return; // Stop execution if validation fails
+    return; 
   }
-   
     try {
       const formData = new FormData();   
       formData.append("phoneNumber", fullPhoneNumber);
@@ -166,7 +150,6 @@ export default function SignIn() {
         localStorage.setItem("authToken", response.data.data.accessToken);
         console.log("here")
         router.push(`/${language}/event`);
-        // return
        setLoader(false)
       } else {
         toast.error(
@@ -180,34 +163,15 @@ export default function SignIn() {
     }
   };
 
-  // const handlePhoneNumberChange = (e) => {
-  //   const input = e.target.value;
-  //     setErrorMessage('');
-  //   // Ensure the input always starts with the selected country code
-  //   if (!input.startsWith(selectedCountryCode)) {
-  //     return; // Prevent any update if the user tries to remove the country code
-  //   }
-  //   // Extract the phone number (part after the country code)
-  //   let numberWithoutCode = input.slice(selectedCountryCode.length);
-  //   numberWithoutCode = numberWithoutCode.replace(/\D/g, '')
-  //   // Update the phone number state without affecting the country code
-  //   setPhoneNumber(numberWithoutCode);
-  // };
-
   const handlePhoneNumberChange = (e) => {
     const value = e.target.value;   
     const cleanedValue = value.replace(/[^0-9]/g, ''); 
-   
-    // Update the phone number state
     setPhoneNumber(cleanedValue);
-
-     // Clear the error message if any
      if (errorMessage) {
       setErrorMessage("");
     }
   };
   
-
   if (loader || firstLoader){
     return <FullPageLoader/>
   }
@@ -218,20 +182,14 @@ export default function SignIn() {
         <div className="2xl:container xl:container lg:container mx-auto lg:max-0  px-5">
           <div className="heading-box text-center xl:mb-11 mb-8">
             <h2 className="xl:text-40 lg:text-[30px] text-[25px] font-bold rtl:2xl:text-[72px] rtl:xl:text-[50px] rtl:text-[40px]">{t("sign_in_now")}</h2>
-            {/* <p className="2xl:text-2xl text-xl font-normal">
-              {t("fill_the_form_below_our_representatives_respond_you")}
-            </p> */}
           </div>
           <div>
             <div className="sm:grid sm:grid-cols-2 ">
             <div className="form-group lg:mb-0 mb-4 mr-4">
               <div className="btn-icon select_country relative flex align-baseline">
-                {
-                  language ==="en" ? 
                   <div>
                  <div className="relative">
                         {/* Overlay that covers the dropdown */}
-
                         <div
                           className="absolute left-0 top-0 z-10 bg-transparent text-[#11171F] flex items-center justify-left p-5 font_32"
                           style={{
@@ -245,81 +203,43 @@ export default function SignIn() {
                         >
                           {selectedCountryCode}
                         </div>
-
                         {/* Actual dropdown */}
                         <select
-                          value={selectedCountryCode}
-                          disabled={isOtpSent || disabledPhoneOTP || OtpMessage}
-                          onChange={(e) => setSelectedCountryCode(e.target.value)}
-                          style={{ opacity: 0 }}
-                          className="max-w-[154px] placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
-                        >
-                          {countries.map((country, index) => (
-                            <option key={index} value={country.code}>
-                              {`${country.name} (${country.code})`}
-                            </option>
-                          ))}
-                        </select>
+                        value={selectedCountryCode}
+                        disabled={isOtpSent || disabledPhoneOTP || OtpMessage}
+                        onChange={(e) => setSelectedCountryCode(e.target.value)}
+                        style={{ opacity: 0 }}
+                        className="max-w-[154px] w-full rounded-[4px] border-2 border-[#DEDEDE] bg-white lg:min-h-[70px] min-h-[50px] text-[#11171F] py-1.5 pr-5 pl-5 lg:text-lg sm:text-sm"
+                      >
+                     {(language === "en" ? countries : arabicCountries).map((country, index) => (
+                    <option key={index} value={country.code}>
+                      {`${country.name} (${country.code})`}
+                    </option>
+                      ))}
+                    </select>
+                      </div>
+                    </div>  
+                <div className="relative w-[100%]">
+                <input
+                  type="number"
+                  inputMode="tel"
+                  name="PhoneNumber"
+                  id="PhoneNumber"
+                value={phoneNumber}
+                  onChange={handlePhoneNumberChange}
+                  className="placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
+                  placeholder="Phone Number"
+                  disabled={isOtpSent || disabledPhoneOTP || OtpMessage} 
+                />
+                </div>
+                <button
+                  disabled={isOtpSent || disabledPhoneOTP || OtpMessage}
+                  onClick={handleSendOTP}
+                  className="px-4 py-2 font-semibold lg:text-lg rounded-[3px] bg-[#1796D8] text-white absolute rtl:xl:text-[30px] lg:w-[149px] w-[100px] lg:top-2 top-[2px] lg:right-2 right-[2px] lg:min-h-[calc(100%-35px)] min-h-[calc(100%-4px)] shadow-shadow-color"
+                >
+                  {t("send_OTP")}
+                </button>
                   </div>
-                </div> :
-                <div>
-                 <div className="relative">
-                        {/* Overlay that covers the dropdown */}
-
-                        <div
-                          className="absolute left-0 top-0 z-10 bg-transparent text-[#11171F] flex items-center justify-center p-5 font_32"
-                          style={{
-                            width: '100%',
-                            height: '100%',
-                            pointerEvents: 'none', 
-                            border: '2px solid #DEDEDE',
-                            borderRadius: '4px',
-                            backgroundColor: '#ffffff',
-                          }}
-                        >
-                          {selectedCountryCode}
-                        </div>
-
-                        {/* Actual dropdown */}
-                        <select
-                          value={selectedCountryCode}
-                          disabled={isOtpSent || disabledPhoneOTP || OtpMessage}
-                          onChange={(e) => setSelectedCountryCode(e.target.value)}
-                          style={{ opacity: 0 }}
-                          className="max-w-[154px] placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
-                        >
-                          {arabicCountries.map((country, index) => (
-                            <option key={index} value={country.code}>
-                              {`${country.name} (${country.code})`}
-                            </option>
-                          ))}
-                        </select>
-                  </div>
-                </div> 
-     }
-             
-            <div className="relative w-[100%]">
-            <input
-              type="number"
-              inputMode="tel"
-              name="PhoneNumber"
-              id="PhoneNumber"
-             value={phoneNumber}
-              // value={`${selectedCountryCode}${phoneNumber}`} // Always shows country code + phone number
-              onChange={handlePhoneNumberChange} // Handles updates without breaking country code
-              className="placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
-              placeholder="Phone Number"
-              disabled={isOtpSent || disabledPhoneOTP || OtpMessage} 
-            />
-             </div>
-             <button
-              disabled={isOtpSent || disabledPhoneOTP || OtpMessage} // Disable immediately on click
-              onClick={handleSendOTP}
-              className="px-4 py-2 font-semibold lg:text-lg rounded-[3px] bg-[#1796D8] text-white absolute rtl:xl:text-[30px] lg:w-[149px] w-[100px] lg:top-2 top-[2px] lg:right-2 right-[2px] lg:min-h-[calc(100%-35px)] min-h-[calc(100%-4px)] shadow-shadow-color"
-            >
-              {t("send_OTP")}
-            </button>
-              </div>
               {
                   errorMessage && (
                 <span className="error_msg">
@@ -377,8 +297,7 @@ export default function SignIn() {
             >
               {t("sign_in")}
             </button>
-            }
-          
+            }  
           </div>
         </div>
       </section>
