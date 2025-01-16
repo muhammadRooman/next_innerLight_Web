c8878ee7977e88a5f7cd51d63c3ff320d492ec75
ArslAkmal <arslan.akmal@arhamsoft.org>
2025-01-16 20:13:01 +0400

    updated content and resolve issues

--- components/DownloadOurApp.js
+++ components/DownloadOurApp.js
@@ -20,11 +20,11 @@ export default function DownloadOurApp() {
                     {t('downloadOurApp')}
                   </h2>
                 </div>
-                <h5 className="2xl:text-[24px] xl:text-[22px] lg:text-[20px] sm:text-[18px] text-[14px] rtl:xl:text-[48px] mb-2 font-bold text-[#753892] heading_40 compassionate">
-                  {t('connectWithExperts')}
-                </h5>
-                <p className="2xl:text-[24px] xl:text-[22px] lg:text-[20px] sm:text-[18px] text-[14px] xl:leading-10 rtl:xl:text-[40px] rtl:max-w-[786px] font-normal mb-4 font_32">
+                <h5 className="2xl:text-[24px] xl:text-[22px] lg:text-[20px] text-[#753892] sm:text-[18px] text-[14px] xl:leading-10 rtl:xl:text-[40px] rtl:max-w-[786px] mb-4 font-bold font_40">
                   {t('personalizedSupport')}
+                </h5>
+                <p className="2xl:text-[24px] xl:text-[22px] lg:text-[20px] sm:text-[18px] text-[14px] rtl:xl:text-[48px] mb-2 text-[#000] font_25 compassionate">
+                  {t('connectWithExperts')}
                 </p>
                 <div className="QR-box mt-9 flex xs:justify-between small:justify-center items-center max-w-[420px] gap-3">
                   <div className="btn-download">
--- components/SignIn/SignIn.js
+++ components/SignIn/SignIn.js
@@ -243,7 +243,7 @@ export default function SignIn() {
                             backgroundColor: '#ffffff',
                           }}
                         >
-                          {selectedCountryCode}
+                          {selectedCountryCode.split('-')[0]}
                         </div>
 
                         {/* Actual dropdown */}
@@ -255,7 +255,7 @@ export default function SignIn() {
                           className="max-w-[154px] placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
                         >
                           {countries.map((country, index) => (
-                            <option key={index} value={country.code}>
+                            <option key={index} value={`${country.code}-${country.name}`}>
                               {`${country.name} (${country.code})`}
                             </option>
                           ))}
@@ -277,7 +277,7 @@ export default function SignIn() {
                             backgroundColor: '#ffffff',
                           }}
                         >
-                          {selectedCountryCode}
+                           {selectedCountryCode.split('-')[0]}
                         </div>
 
                         {/* Actual dropdown */}
@@ -289,7 +289,7 @@ export default function SignIn() {
                           className="max-w-[154px] placeholder:text-[#11171F] w-full items-center dir_left-t-right rounded-[4px] bg-white border-solid border-2 border-[#DEDEDE] outline-1 -outline-offset-1 outline-[#DEDEDE] focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#11171F] lg:min-h-[70px] min-h-[50px] block min-w-0 grow py-1.5 pr-5 pl-5 lg:text-lg text-[#11171F] focus:outline-none rtl:xl:text-[32px] sm:text-sm/6"
                         >
                           {arabicCountries.map((country, index) => (
-                            <option key={index} value={country.code}>
+                            <option key={index} value={`${country.code}-${country.name}`}>
                               {`${country.name} (${country.code})`}
                             </option>
                           ))}
